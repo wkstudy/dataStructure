9ff538369388b70185a9730031232853599afd69
wkstudy <1346948470@qq.com>
2018-11-15 21:18:24 +0800

    栈

---
+++ Stack/es6.js
@@ -0,0 +1,54 @@
+class Stack {
+  constructor () {
+    this.items = []
+  }
+  push (ele) {
+    this.items.push(ele)
+  }
+  pop () {
+    this.items.pop()
+  }
+  peek () {
+    // 返回栈顶元素，不做任何修改
+    return this.items[this.items.length - 1]
+  }
+  isEmpty () {
+    return this.items.length == 0
+  }
+  clear () {
+    this.items = []
+  }
+  size () {
+    return this.items.length
+  }
+}
+
+// let stack = new Stack();
+// console.log(stack.isEmpty()); //输出为true 
+// stack.push(5);
+// stack.push(8); 
+// console.log(stack.peek()); //输出8 
+// stack.push(11);
+// console.log(stack.size()); //输出3
+// console.log(stack.isEmpty()); //输出false 
+
+
+// 十进制到任意进制（这里的禁止必须小于10，因为大与10进制的话会有字母）  
+
+// Math.floor() 向下取整
+function  transform (num, d) {
+  var stack = new Stack();
+  while (Math.floor(num / d) != 0) {
+    stack.push(num % d);
+    num =Math.floor(num / d);
+  }
+  var result = num.toString();
+  while (!stack.isEmpty()) {
+    result += stack.peek();
+    stack.pop();
+  }
+  return result;
+}
+
+ var x = transform(10, 8);
+ console.log(x)
